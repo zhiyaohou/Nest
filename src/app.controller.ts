745f0c6b9cf9f64496cfe07ca1f6b4de6adc784f
zhiyaohou <60085736+zhiyaohou@users.noreply.github.com>
2022-01-10 23:03:35 -0500

    crud-fail
    fail

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,8 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Delete, Patch, Param} from '@nestjs/common';
 import { AppService } from './app.service';
+
+let blog: string = "offers come.";
+let secblog: string = "this is a blog.";
 
 @Controller()
 export class AppController {
@@ -9,4 +12,25 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get()
+  listBlog(@Param() params): string {
+    return "";
+  }
+
+  @Post()
+  createBlog(): string{
+    return this.appService.createBlog();
+  }
+
+  @Delete()
+  delteBlog(): string{
+    return this.appService.deleteBlog();
+  }
+
+  @Patch()
+  updateBlog(): string{
+    return this.appService.updateBlog();
+  }
+
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,20 @@ export class AppService {
   getHello(): string {
     return 'Hello World Good!';
   }
+
+  listBlog(): string{
+    return 'list blog';
+  }
+
+  createBlog(): string {
+    return 'new blog';
+  }
+
+  deleteBlog(): string {
+    return 'new blog';
+  }
+
+  updateBlog(): string{
+    return 'new blog';
+  }
 }
